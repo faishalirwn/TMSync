5dade088b37f7886ca129faa5a049c45278552a5
Faishal Irawan <muhammadfaishal193@gmail.com>
2024-07-17 22:47:04 +0700

    add binged.live vidsrc.pro inefficient dirty spaghet

--- src/content.js
+++ src/content.js
@@ -1,23 +1,23 @@
 const url = window.location.href;
 const urlObj = new URL(url);
 
-const getMediaType = (url) => {
-    let type;
-    const urlObj = new URL(url);
-    const urlPath = urlObj.pathname.split('/');
-
-    if (urlPath[1] === 'movie') {
-        type = 'movie';
-    } else if (urlPath[1] === 'tv') {
-        type = 'show';
-    }
-    return type;
-};
-
 if (
     urlObj.hostname === 'fmovies24.to' &&
     (urlObj.pathname.startsWith('/tv') || urlObj.pathname.startsWith('/movie'))
 ) {
+    const getMediaType = (url) => {
+        let type;
+        const urlObj = new URL(url);
+        const urlPath = urlObj.pathname.split('/');
+
+        if (urlPath[1] === 'movie') {
+            type = 'movie';
+        } else if (urlPath[1] === 'tv') {
+            type = 'show';
+        }
+        return type;
+    };
+
     const title = document.querySelector('h1.name').textContent;
     const year = document.querySelector('span.year').textContent ?? undefined;
 
@@ -27,13 +27,6 @@ if (
             ' '
         )[0] ?? undefined;
 
-    // const genreSpan = document.querySelector(
-    //     'div.detail > div:nth-child(3) > span'
-    // ).textContent;
-    // const genres =
-    //     genreSpan.split(',').map((genre) => genre.trim().toLowerCase()) ??
-    //     undefined;
-
     (async () => {
         const resp = await chrome.runtime.sendMessage({
             type: 'mediaInfo',
@@ -41,7 +34,89 @@ if (
                 type: getMediaType(url),
                 query: title || undefined,
                 years: year,
-                runtimes,
+                runtimes
+            }
+        });
+        console.log(resp);
+    })();
+}
+
+function waitForElm(selector) {
+    return new Promise((resolve) => {
+        if (document.querySelector(selector)) {
+            return resolve(document.querySelector(selector));
+        }
+
+        const observer = new MutationObserver(() => {
+            if (document.querySelector(selector)) {
+                observer.disconnect();
+                resolve(document.querySelector(selector));
+            }
+        });
+
+        // If you get "parameter 1 is not of type 'Node'" error, see https://stackoverflow.com/a/77855838/492336
+        observer.observe(document.body, {
+            childList: true,
+            subtree: true
+        });
+    });
+}
+
+if (urlObj.hostname === 'binged.live' && urlObj.pathname.startsWith('/watch')) {
+    const getMediaType = (url) => {
+        let type;
+        const urlObj = new URL(url);
+        const urlPath = urlObj.pathname.split('/');
+
+        if (urlPath[2] === 'movie') {
+            type = 'movie';
+        } else if (urlPath[2] === 'tv') {
+            type = 'show';
+        }
+        return type;
+    };
+
+    (async () => {
+        let title;
+        let year;
+        if (getMediaType(url) === 'movie') {
+            const titleEl = await waitForElm(
+                // prettier-ignore
+                // eslint-disable-next-line no-useless-escape
+                '#root > div.min-h-screen.overflow-auto > div:nth-child(2) > div.w-full.max-w-7xl.mx-auto.px-2.sm\\:px-3 > div > div.flex.w-full.lg\\:w-\\[70\\%\\].md\\:w-\\[80\\%\\].flex-col.gap-2 > span'
+            );
+            title = titleEl.textContent;
+
+            const yearEl = await waitForElm(
+                // prettier-ignore
+                // eslint-disable-next-line no-useless-escape
+                'div.min-h-screen.overflow-auto > div:nth-child(2) > div.w-full.max-w-7xl.mx-auto.px-2.sm\\:px-3 > div > div.flex.w-full.lg\\:w-\\[70\\%\\].md\\:w-\\[80\\%\\].flex-col.gap-2 > div.flex.flex-col.flex-wrap.gap-\\[\\.6rem\\].lg\\:gap-\\[\\.4rem\\].tracking-wide > div:nth-child(1) > span'
+            );
+            year = yearEl.textContent.split('/')[0] ?? undefined;
+        }
+
+        if (getMediaType(url) === 'show') {
+            const titleEl = await waitForElm(
+                // prettier-ignore
+                // eslint-disable-next-line no-useless-escape
+                '#root > div:nth-child(7) > div > div.flex.w-full.lg\\:w-\\[70\\%\\].md\\:w-\\[80\\%\\].flex-col.gap-2 > span'
+            );
+            title = titleEl.textContent;
+
+            const yearEl = await waitForElm(
+                // prettier-ignore
+                // eslint-disable-next-line no-useless-escape
+                '#root > div:nth-child(7) > div > div.flex.w-full.lg\\:w-\\[70\\%\\].md\\:w-\\[80\\%\\].flex-col.gap-2 > div.flex.flex-col.flex-wrap.gap-\\[\\.6rem\\].lg\\:gap-\\[\\.4rem\\].tracking-wide > div:nth-child(1) > span'
+            );
+            year = yearEl.textContent.split('/')[0] ?? undefined;
+        }
+
+        const resp = await chrome.runtime.sendMessage({
+            type: 'mediaInfo',
+            payload: {
+                type: getMediaType(url),
+                query: title || undefined,
+                years: year
             }
         });
         console.log(resp);
--- src/utils/url.js
+++ src/utils/url.js
@@ -1,15 +1,46 @@
 export const getUrlIdentifier = (url) => {
     const urlObj = new URL(url);
     const urlPath = urlObj.pathname.split('/');
-    return urlObj.hostname + '/' + urlPath[1] + '/' + urlPath[2];
+
+    if (
+        urlObj.hostname === 'binged.live' &&
+        urlObj.pathname.startsWith('/watch')
+    ) {
+        return urlObj.hostname + '/' + urlObj.pathname;
+    }
+
+    if (
+        urlObj.hostname === 'fmovies24.to' &&
+        (urlObj.pathname.startsWith('/tv') ||
+            urlObj.pathname.startsWith('/movie'))
+    ) {
+        return urlObj.hostname + '/' + urlPath[1] + '/' + urlPath[2];
+    }
 };
 
 export const getEpisode = (url) => {
     const urlObj = new URL(url);
     const urlPath = urlObj.pathname.split('/');
-    const episodeArr = urlPath[urlPath.length - 1].split('-');
-    return {
-        season: episodeArr[0],
-        number: episodeArr[1]
-    };
+
+    if (
+        urlObj.hostname === 'binged.live' &&
+        urlObj.pathname.startsWith('/watch')
+    ) {
+        return {
+            season: urlObj.searchParams.get('season'),
+            number: urlObj.searchParams.get('ep')
+        };
+    }
+
+    if (
+        urlObj.hostname === 'fmovies24.to' &&
+        (urlObj.pathname.startsWith('/tv') ||
+            urlObj.pathname.startsWith('/movie'))
+    ) {
+        const episodeArr = urlPath[urlPath.length - 1].split('-');
+        return {
+            season: episodeArr[0],
+            number: episodeArr[1]
+        };
+    }
 };
